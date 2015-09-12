c25f5eef7985ea99fc04a496edc4e9702d7e8c93
Evan Hamlin <evan.hamlin1@gmail.com>
2015-09-12 09:31:03 -0700

    project mockup, none of the 'pi' functionality

---
+++ mockup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+import sys, time
+# import RPI.GPIO as GPIO
+
+# constants
+myVar = "\nHowdy!"
+
+# exception classes
+# interface functions
+# classes
+# internal functions & classes
+
+def main():
+    print (myVar)
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
