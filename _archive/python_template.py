affb82245e7ae0e4b248b25b48629d04b37c8f61
Talapus <noumenaut@gmail.com>
2015-10-03 09:47:14 -0700

    python template archived

---
+++ _archive/python_template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+import sys
+
+# constants
+myVar = "\nHowdy!"
+
+# exception classes
+# interface functions
+# classes
+# internal functions & classes
+
+def main():
+    print (myVar)
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
